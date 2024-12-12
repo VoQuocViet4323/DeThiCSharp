a735c9a9043a376fe16b35c85ef5189685299363
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-12-12 13:43:35 +0700

    aaaa

---
+++ DeThi/Controllers/BenhNhanController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DeThi.Models;
+
+namespace DeThi.Controllers
+{
+    public class BenhNhanController : Controller
+    {
+        private QLBVEntities db = new QLBVEntities();
+
+        // GET: BenhNhan
+        public ActionResult Index()
+        {
+            return View(db.BenhNhan.ToList());
+        }
+
+        // GET: BenhNhan/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BenhNhan benhNhan = db.BenhNhan.Find(id);
+            if (benhNhan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(benhNhan);
+        }
+
+        // GET: BenhNhan/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BenhNhan/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaBN,HoTen,NgaySinh,GioiTinh,DienThoai")] BenhNhan benhNhan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BenhNhan.Add(benhNhan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(benhNhan);
+        }
+
+        // GET: BenhNhan/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BenhNhan benhNhan = db.BenhNhan.Find(id);
+            if (benhNhan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(benhNhan);
+        }
+
+        // POST: BenhNhan/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaBN,HoTen,NgaySinh,GioiTinh,DienThoai")] BenhNhan benhNhan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(benhNhan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(benhNhan);
+        }
+
+        // GET: BenhNhan/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BenhNhan benhNhan = db.BenhNhan.Find(id);
+            if (benhNhan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(benhNhan);
+        }
+
+        // POST: BenhNhan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BenhNhan benhNhan = db.BenhNhan.Find(id);
+            db.BenhNhan.Remove(benhNhan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DeThi/Controllers/LichHenController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DeThi.Models;
+
+namespace DeThi.Controllers
+{
+    public class LichHenController : Controller
+    {
+        private QLBVEntities db = new QLBVEntities();
+
+        public ActionResult Index(string TenBN, string TenBS)
+        {
+            var lichHen = db.LichHen.Include(d => d.BenhNhan).Include(d => d.BacSi).Where(c => c.BenhNhan.HoTen.Contains(TenBN));
+            ViewBag.TenBS = db.LichHen.Include("BacSi").Where(c => c.BacSi.TenBS == TenBS).Select(
+                g => new SelectListItem
+                {
+                    Text = g.BacSi.TenBS,
+                    Value = g.BacSi.TenBS
+                }
+            ).ToList();
+            return View(lichHen.ToList());
+        }
+
+
+        // GET: LichHen/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LichHen lichHen = db.LichHen.Find(id);
+            if (lichHen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lichHen);
+        }
+
+        // GET: LichHen/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaBS = new SelectList(db.BacSi, "MaBS", "TenBS");
+            ViewBag.MaBN = new SelectList(db.BenhNhan, "MaBN", "HoTen");
+            return View();
+        }
+
+        // POST: LichHen/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaLH,MaBN,MaBS,NgayHen,LyDo")] LichHen lichHen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LichHen.Add(lichHen);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaBS = new SelectList(db.BacSi, "MaBS", "TenBS", lichHen.MaBS);
+            ViewBag.MaBN = new SelectList(db.BenhNhan, "MaBN", "HoTen", lichHen.MaBN);
+            return View(lichHen);
+        }
+
+        // GET: LichHen/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LichHen lichHen = db.LichHen.Find(id);
+            if (lichHen == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaBS = new SelectList(db.BacSi, "MaBS", "TenBS", lichHen.MaBS);
+            ViewBag.MaBN = new SelectList(db.BenhNhan, "MaBN", "HoTen", lichHen.MaBN);
+            return View(lichHen);
+        }
+
+        // POST: LichHen/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaLH,MaBN,MaBS,NgayHen,LyDo")] LichHen lichHen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lichHen).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaBS = new SelectList(db.BacSi, "MaBS", "TenBS", lichHen.MaBS);
+            ViewBag.MaBN = new SelectList(db.BenhNhan, "MaBN", "HoTen", lichHen.MaBN);
+            return View(lichHen);
+        }
+
+        // GET: LichHen/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LichHen lichHen = db.LichHen.Find(id);
+            if (lichHen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lichHen);
+        }
+
+        // POST: LichHen/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LichHen lichHen = db.LichHen.Find(id);
+            db.LichHen.Remove(lichHen);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
